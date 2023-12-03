6d37bc353bfafe1b568324699eb10468b5d0356c
abhinavlakkaraj <63047126+abhinavlakkaraj@users.noreply.github.com>
2023-12-02 22:47:15 -0500

    Delete Abhinav-Lakkarak_project_final directory

--- Abhinav-Lakkarak_project_final/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost","root","","car_comparison");
-
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-?>
